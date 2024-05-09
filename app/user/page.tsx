6dbc74093b1e51c29470d3213741edac8cbbcde1
Mayki <aw8hurtz@gmail.com>
2024-05-09 11:41:28 +0800

    recent commit addtionl

---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <>
+      <div>UsersPage</div>
+      <Link href="/">Go back</Link>
+    </>
+  );
+}
+
+export default UsersPage
