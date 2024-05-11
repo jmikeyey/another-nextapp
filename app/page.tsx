d65f6c8c2091117414f58309a9d5324b4fdb1727
Mayki <aw8hurtz@gmail.com>
2024-05-11 11:58:59 +0800

    dynamic routing sample

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
-      <Link href='/user'>Go to users</Link>
+      <Link href="/user">Go to users</Link> <br />
+      <Link href="/product">Go to products</Link>
     </main>
   );
 }
---
+++ app/product/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from "next/link";
+// Sample array of products with ids and names
+const products = [
+  { id: 1, name: 'Product 1' },
+  { id: 2, name: 'Product 2' },
+  { id: 3, name: 'Product 3' }
+];
+const Products = () => {
+  return (
+    <>
+      <Link href="/">Go back</Link>
+      <hr />
+      <h1 className="bg-sky-400 px-1">Product List</h1>
+      <div className="flex flex-col">
+        {/* Map through the products array */}
+        {products.map((product) => (
+          // Use Link component to dynamically generate links
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+            passHref
+            legacyBehavior
+          >
+            <h1 className="hover:bg-gray-200 px-1 py-2">{product.name}</h1>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Products
